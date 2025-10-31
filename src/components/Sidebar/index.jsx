b46b7680528ed995e4e9a953becefcea346f2227
Giordano Bruno Biasi Berwig <giordano.berwig@sou.unijui.edu.br>
2025-10-31 11:42:08 -0300

    fix: updated the Sidebar component to pass the props to SidebarAdministrationComponent

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -2,8 +2,11 @@ import logo from '../../assets/logo.png'
 import {SidebarAdministrationMenu} from "../SidebarAdministrationMenu/index.jsx";
 import {SidebarUserAccountBadge} from "../SidebarUserAccountBadge/index.jsx";
 import {SidebarLinks} from "../SidebarLinks/index.jsx";
+import {useState} from "react";
 
 export function Sidebar() {
+    const [adminOpen, setAdminOpen] = useState(false)
+
     return (
         <aside className="w-[250px] p-4 bg-background-secondary h-[100vh] font-primary flex flex-col justify-between rounded-tr-2xl rounded-br-2xl">
             <div>
@@ -11,7 +14,7 @@ export function Sidebar() {
                     <img className='w-[200px]' src={logo} alt="Hospital Environment Control"/>
                 </a>
                 <SidebarLinks/>
-                <SidebarAdministrationMenu/>
+                <SidebarAdministrationMenu adminOpen={adminOpen} setAdminOpen={setAdminOpen} />
             </div>
             <SidebarUserAccountBadge/>
         </aside>
