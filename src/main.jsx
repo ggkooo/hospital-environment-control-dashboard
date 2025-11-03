fb96aea4c21229462587b0033c5a6700ad1a99d1
Giordano Bruno Biasi Berwig <giordano.berwig@sou.unijui.edu.br>
2025-11-03 09:40:17 -0300

    fix: updated main.jsx to use React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {BrowserRouter, Route, Routes} from 'react-router'
 import './index.css'
 import App from './App.jsx'
+import {Home} from "./pages/Home/index.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <BrowserRouter>
+          <Routes>
+              <Route path='/' element={<Home/>}/>
+
+              <Route path='/administration'>
+                  <Route path='users' element={<App/>}/>
+                  <Route path='access-log' element={<App/>}/>
+                  <Route path='reports-manager' element={<App/>}/>
+              </Route>
+          </Routes>
+      </BrowserRouter>
   </StrictMode>,
 )
